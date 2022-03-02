2243e4afdabb056550b76eec52b34329663781c6
Daniel Rozmajzl <daniel.rozmajzl@gmail.com>
2022-03-02 11:57:14 -0500

    commit tree 767bb09238b0c99061083bba26afdff579ada073

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    x = self.actors
+        x.map{|y| "#{y.first_name} #{y.last_name}"}
+  end
 end
